d0de4158dec332f3d1cd28a43d2eb5ad51b262de
Kyaa-A <asnaripacalna@gmail.com>
2022-11-23 19:59:42 +0700

    init

---
+++ test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+    //Exercise 4 ― Odd and Even using array
+    /* Ask 10 numbers from the user then determine the odd and even numbers entered by the user */
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num[] = new int[10];
+        ArrayList <Integer> evenNum = new ArrayList<Integer>();
+        ArrayList <Integer> oddNum = new ArrayList<Integer>();
+
+        
+
+    }
+}
