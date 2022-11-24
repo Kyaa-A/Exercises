56b89627e7139c83de8651d5a09ec089b8d31d11
Kyaa-A <asnaripacalna@gmail.com>
2022-11-24 10:55:24 +0800

    init

---
+++ practice4v2.java
@@ -0,0 +1,34 @@
+public class practice4v2 {
+    public static void main(String[] args) {
+        
+        // First
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+
+        // Second
+        for (int i = 0; i <= 5; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+        
+        // Third
+        for (int i = 0; i <= 5; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+
+
+
+    }
+}
