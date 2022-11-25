ba8b4029e5cffad81aeaa5ffb7fe833ed5437a48
Kyaa-A <asnaripacalna@gmail.com>
2022-11-25 20:05:42 +0700

    init

--- problem3.java
+++ problem3.java
@@ -4,46 +4,45 @@ public class problem3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-/*         System.out.println("Input: ");
+        System.out.println("Input:");
+        System.out.print("Enter the amount of change from 1 to 99:\t");
+        int num = scan.nextInt();
+        int cents = num;
 
-        int num = 0;
-        System.out.print("Enter the amoung of change from 1 to 99: ");
-        num = scan.nextInt();
+        int quarter = cents / 25;
+        cents %= 25;
 
-        int quarter = num / 25;
-
+        int dime = cents / 10;
+        cents %= 10;
+        
+        int nickels = cents / 5;
+        cents %= 5;
+        
+        int penny = cents;
 
         System.out.println("\nOutput: ");
-        System.out.println("\t" + num + " Cents in coins: ");
-
-        System.out.println("\t" + quarter + " quarters\t\t" + (quarter*25));
-
-        int dime = quarter / 10;
-        System.out.println("\t" + dime + " dime\t\t" + (dime*10)); */
-
-        int num;
-        System.out.print("Enter the amount of change from 1 to 99: ");
-        num = scan.nextInt();
-
-        int quarter = num / 25;
-
-        int dime = num / 10;
-
-        int nickels = num / 5;
-
-        int penny = num / 1;
-
 
         if(num>=1 && num<=99){
+            System.out.println("\t" + num + " Cents in coins:");
+
             if(num>=25){
                 System.out.println("\t" + quarter + " quarters\t\t" + (quarter*25));
                 
                 if(num>=10){
                     System.out.println("\t" + dime + " dime\t\t\t" + (dime*10));
+
+                    if(num>=5){
+                        System.out.println("\t" + nickels + " nickles\t\t" + (nickels*5));
+
+                        if(num>=1){
+                            System.out.println("\t" + penny + " penny\t\t\t" + penny);
+                        }
+                    }
                 }
             }
         }else{
             System.out.println("\nInvalid-Maximum is only 1-99: " + num);
         }
+        System.out.println();
     }
 }
