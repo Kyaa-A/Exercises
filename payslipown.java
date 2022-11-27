3c151aa497281868cc8d1a116b32fa60b4d15593
Kyaa-A <asnaripacalna@gmail.com>
2022-11-27 10:44:48 +0700

    init

---
+++ payslipown.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class payslipown {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        String employee[][] = {
+            {"0121","Joane May Delima"},
+            {"0122","Juan Dela Cruz"},
+            {"0123","Koko"},
+            {"0124","Jojo"}
+        };
+        
+        
+
+
+    }
+}
