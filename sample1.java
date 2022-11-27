efc0fa15787b5e80e3e7055863ed63b8814de4da
Kyaa-A <asnaripacalna@gmail.com>
2022-11-27 07:48:28 +0700

    init

--- sample1.java
+++ sample1.java
@@ -1,54 +1,16 @@
 import java.util.Scanner;
 
-public class sample1 {
-    
-    //Exercise 4 ― Odd and Even using array
-    /* Ask 10 numbers from the user then determine the odd and even numbers entered by the user */
-
+public class sample1{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        String info[] = {"Name","Gender","Address"};
 
-        int[] num = new int[10];
-        int[] evenNum = new int[10];
-        int[] oddNum = new int[10];
 
-        System.out.println("***********INPUT***********");
-        System.out.println();
-
-        for (int i = 0; i < num.length; i++) {
-            System.out.print("Enter a number:\t");
-            num[i] = scan.nextInt();
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Input your " + info[i] + ":\t");
+            info[i] = scan.nextLine();
         }
-        scan.close();
-
-        System.out.println();
-        System.out.println("***********OUTPUT***********");
-        System.out.println();
-        System.out.print("Even Elements:\t");
-        for (int i = 0; i < evenNum.length-1; i++) {
-            if (num[i]%2 == 0){
-                evenNum[i] = num[i];
-                if (i >= 0) {
-                    System.out.print(evenNum[i]+ ", ");
-                }
-            }
-        }
-        System.out.print(evenNum[num.length-1]);
-        System.out.println();
-
-        System.out.print("Odd Elements:\t");
-        for (int i = 1; i < oddNum.length; i++) {
-            if (num[i]%2 == 1){
-                oddNum[i] = num[i];
-            }
-            if (oddNum[i] != 0) {
-                if (i >= 0) {
-                    System.out.print(oddNum[i]+", ");
-                }
-            }
-        }
-        System.out.print(oddNum[oddNum.length-1]);
-        System.out.println();
+       System.out.println("Your name is " + info[0] + " and you are a " + info[1] + " and you live in " + info[2] );
     }
-
 }
