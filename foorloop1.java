dccc8e272da4967ceee786182edc1880bf170977
Kyaa-A <asnaripacalna@gmail.com>
2022-12-07 22:52:09 +0700

    init

--- foorloop1.java
+++ foorloop1.java
@@ -1,8 +1,34 @@
+
+// Adding Up Integers
+
+/* Write a program that add integers that the user enters. First the 
+ * program asks how many numbers will be added up. then the 
+ * program propmts the user for each number.
+ * Finally it prints the sum. You may use any looping structures.
+ */
+
+
+import java.util.Scanner;
+
 public class foorloop1{
     public static void main(String[] args) {
         
-        for (int i = 5; i <= 50; i+=5) {
-            System.out.println(i);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("How many integers will be added: ");
+        int total = scan.nextInt();
+        System.out.println();
+        
+        int sum = 0;
+        for (int i = 0; i < total; i++) {
+            System.out.print("Enter an Integer:\t");
+            int num = scan.nextInt();
+            sum += num;
         }
+
+        System.out.println("Total: " + sum);
     }
 }
+
+
+
